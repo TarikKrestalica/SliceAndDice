3fa5094590223620807f15eb8bd54beb69229298
madhavpatel <madhav2003patel@gmail.com>
2024-03-26 03:21:00 -0400

    LEVEL DESIGN AND TITLE SCREENS DONE
    timer not done yet still working on it

--- SliceAndDice/Assets/Scripts/MainMenu.cs
+++ SliceAndDice/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene(2);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Controls()
     {
-        
+        SceneManager.LoadScene(1);
+
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
 }
